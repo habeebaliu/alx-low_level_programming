07a52936260439bc034881c0e8f170277e0eaada
Aliu Opeyemi Habeeb <aliuhabeeb03@gmail.com>
2023-02-16 18:44:45 +0000

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
+
 /**
- * main - A program that print a line with put function
- * Returns: 0
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");
-return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
