c579bf91012af979d60cbf1b1f7ca26b36069a46
Aliu Opeyemi Habeeb <aliuhabeeb03@gmail.com>
2023-02-16 19:27:56 +0000

    Update 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 #include <unistd.h>
-/**
- * main - A C program that print a line to a standard error
- * Return 1
- */
 
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-	fput ("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",stdout);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
