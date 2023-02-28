e2f13d87c6e91d57c6d0fa9cf95ad39f8e4facd9
Aliu Opeyemi Habeeb <aliuhabeeb03@gmail.com>
2023-02-28 00:48:03 +0000

    Update 4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,16 +1,18 @@
 #include "main.h"
 
 /**
- *  * print_rev - Prints a string in reverse.
- *   * @s: string to tbe reserved.
+ * print_rev - Prints a string in reverse.
+ * @s: string to tbe reserved.
  */
 void print_rev(char *s)
 {
-		int len = 0, i = 0;
+	int len = 0, i = 0;
 
-		while (s[i++])
+	while (s[i++])
 		len++;
-		for (i = len - 1; i >= 0; i--)
-			_putchar(s[i]);
-			_putchar('\n');
+
+	for (i = len - 1; i >= 0; i--)
+		_putchar(s[i]);
+
+	_putchar('\n');
 }
