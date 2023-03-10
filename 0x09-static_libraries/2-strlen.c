adfa9f065e523c7ce4940ff2c38bafff5596031b
habeebaliu <aliuhabeeb03@gmail.com>
2023-03-10 15:01:30 -0800

    static

---
+++ 0x09-static_libraries/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ *  * _strlen - Returns the length of a string.
+ *   * @str: string.
+ *    *
+ *     * Return: Length.
+ */
+int _strlen(char *str)
+{
+		int len = 0;
+
+		while (*str++)
+			len++;
+		return (len);
+}
