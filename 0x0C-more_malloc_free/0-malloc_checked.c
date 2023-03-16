7b6d0b3bf3ab9223c5783e90783ea4746eb55c75
Aliu Opeyemi Habeeb <aliuhabeeb03@gmail.com>
2023-03-16 16:52:35 +0000

    Create 0-malloc_checked.c
    More malloc free

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,15 +1,16 @@
+
 #include "main.h"
 #include <stdlib.h>
 
 /**
- * malloc_checked - allocates memory using malloc
+ * malloc_checked - allocates memoty using malloc
  * @b: function int parameter
  * Return: void
  */
 
 void *malloc_checked(unsigned int b)
 {
-	void *c = malloc (b);
+	void *c = malloc(b);
 
 	if (c == NULL)
 	{
