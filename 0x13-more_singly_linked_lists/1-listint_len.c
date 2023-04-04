dde61112ddc5de9af2bb44f439aa4d62a4567deb
habeebaliu <aliuhabeeb03@gmail.com>
2023-04-04 02:06:52 -0700

    create 1-listint_len.c file

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * listint_len - Return the number of element in the
+ * linked list
+ * @h: A pointer to the head of the listint_list
+ *
+ * Return: The number of elements in the listint_list
+ */
+
+size_t listint_len(const listint_t *h)
+{
+	size_t node = 0;
+
+	while (h)
+	{
+		nodes++;
+		h = h->head;
+	}
+
+	return (nodes);
+
+}
